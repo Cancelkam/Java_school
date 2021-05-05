3598a01b984bfdcc9cc3a2900932787f85b574a5
ekochnev <Elena.Kochneva@telekom.com>
2021-05-05 15:33:34 +0300

    tariff view added

--- src/main/java/com/javaSchool/eCare/controller/MainController.java
+++ src/main/java/com/javaSchool/eCare/controller/MainController.java
@@ -3,6 +3,7 @@ package com.javaSchool.eCare.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -14,4 +15,16 @@ public class MainController {
 
         return "index";
     }
+
+    @GetMapping(value = {"/login"})
+    public String loginPage(@RequestParam(required = false) String login, Model model) {
+        model.addAttribute("login" , login);
+        return "login";
+    }
+    @GetMapping(value = {"/tariffs"})
+    public String tariffPage(@RequestParam(required = false) String tariff, Model model) {
+        model.addAttribute("tariff" , tariff);
+        return "tariffs";
+    }
+
 }
